83421359abb21a914229b22274f860cde98aebbf
HelenBrando <2552965905@qq.com>
2025-08-11 03:04:12 +0000

    base基准脚本更新

--- script/train/fgclip_train.sh
+++ script/train/fgclip_train.sh
@@ -1,14 +1,24 @@
-#!/bin/bash
 DATASET_NAME="ORBench"
 
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=7 \
 python train.py \
---name irra \
+--batch_size 24 \
+--loss_name 'multi_modal_contrastive+itc' \
+--sampler random \
+--pretrain_choice '/SSD_Data01/zyl/prcv_work/model_cache/huggingface_model/model.safetensors' \
+--test_size 0.125 \
+--eval_period 20 \
+--drop_last 1 \
 --img_aug \
---batch_size 16 \
 --MLM \
 --dataset_name $DATASET_NAME \
---loss_names 'sdm+id' \
---num_epoch 60 \
---pretrain_choice '/SSD_Data01/zyl/prcv_work/model_cache/fgclip/model.safetensors' \
---root_dir '/SSD_Data01/PRCV-ReID5o/data/'
+--name fgclip \
+--root_dir '/SSD_Data01/PRCV-ReID5o/data/' \
+--num_epoch 1000 \
+--lr 2.4e-5 \
+--warmup_epochs 580 \
+--lrscheduler exp \
+--power 0.5 \
+--step_size 2000 \
+--add_multimodal_layers \
+--img_size 224,224 \
